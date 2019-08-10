e19443c7b852d76f4b9a55c381a5dff8fec4f533
Paulo Jerônimo <paulojeronimo@gmail.com>
2019-08-10 07:56:40 -0300

    Created `durationToSeconds` arrow function. Updated code to ES6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-var parse=require('parse-duration')
-
-console.log(parse('1h 20 min'))
+const parse=require('parse-duration')
+exports.durationToSeconds = time => parse(time) / 1000
+const duration='1h 20 min'
+console.log(`${duration} is equivalent to ${exports.durationToSeconds(duration)} seconds`)
