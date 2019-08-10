02ddd07c895f607bd34232daf69acad0005cd9d5
Paulo Jerônimo <paulojeronimo@gmail.com>
2019-08-10 11:56:11 -0300

    Updated to test with jest

--- index.js
+++ index.js
@@ -2,5 +2,3 @@
 
 const parse=require('parse-duration')
 exports.durationToSeconds = time => parse(time) / 1000
-const duration='1h 20 min'
-console.log(`${duration} is equivalent to ${exports.durationToSeconds(duration)} seconds`)
